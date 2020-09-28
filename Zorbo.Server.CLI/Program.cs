261074563053d4539e30f53cd1598b1e876499fa
itsdeandavidson <6406194+itsdeandavidson@users.noreply.github.com>
2020-09-28 02:30:10 -0700

    Added a shitty CLI & changed postbuild events (now works cross-platform).

---
+++ Zorbo.Server.CLI/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Loader;
+using System.Threading;
+using Zorbo.Ares.Server;
+using Zorbo.Core;
+using Zorbo.Core.Models;
+
+
+namespace Zorbo.Server.CLI
+{
+    class Program
+    {
+        static AresServer server;
+        static ManualResetEvent shutdown = new ManualResetEvent(false);
+
+
+        static int Main(string[] args)
+        {
+            Console.Title = "Zorbo";
+
+            var config = Persistence.LoadModel<AresServerConfig>(Path.Combine(Directories.AppData, "config.json"));
+            server = new AresServer(config);
+
+            server.PluginHost.LoadPlugin("Autoload");
+
+            if (server.Config.AutoStartServer)
+                server.Start();
+
+            AssemblyLoadContext.Default.Unloading += Default_Unloading;
+
+            shutdown.WaitOne();
+
+            return 0;
+        }
+
+        private static void Default_Unloading(AssemblyLoadContext obj)
+        {
+            if (server.Running)
+            {
+                server.Stop();
+            }
+
+            shutdown.Set();
+        }
+    }
+}
